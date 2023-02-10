1ec0fa63271620f09fdd41f11a7966f427d0f6a1
Parazit15D <igorlyons@gmail.com>
2023-02-10 22:07:46 +0200

    hello

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ book = openpyxl.load_workbook("Price.xlsx", read_only=True)
 sheet = book.active
 
 print("We Work")
+print("Hello")
 
 root = minidom.Document()
 xml = root.createElement('ARTICLES')
